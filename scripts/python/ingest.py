e7c3ee433357aeb24ec55ba168fc807db42f56c7
cmill25 <44711910+cmill25@users.noreply.github.com>
2022-11-03 15:45:21 -0500

    Update ingest.py

--- scripts/python/ingest.py
+++ scripts/python/ingest.py
@@ -1,1 +1,18 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+import logging
+
+logger = logging.getLogger(__name__)
+  
+  
+conn_string = 'postgres://user:password@host/data1'
+  
+db = create_engine(conn_string)
+conn = db.connect()
+  
+def load_to_sql(self, table):
+    conn = psycopg2.connect(conn_string)
+    conn.autocommit = True
+    table.to_sql(f'stg.{table}', con=conn, if_exists='replace', index=False)
 
