75f33ecf9e49e6ed051aa5b42f5229358d7ec96e
cmill25 <44711910+cmill25@users.noreply.github.com>
2022-11-03 16:23:10 -0500

    Update ingest.py

--- scripts/python/ingest.py
+++ scripts/python/ingest.py
@@ -16,3 +16,6 @@ def load_to_sql(self, table):
     conn.autocommit = True
     table.to_sql(f'stg.{table}', con=conn, if_exists='replace', index=False)
 
+if df.columnName.isnull().any():
+    logging.warning('Following items missing for File > %s' + '\n' + str(
+        df.loc[df['columnName'].isnull()].iloc[:, [1,2,3,4]]) + '\n', file_name)
