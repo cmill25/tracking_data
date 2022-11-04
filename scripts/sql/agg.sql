b1cc32a5ba4f1086373414ede9d574f527fcebaa
cmill25 <44711910+cmill25@users.noreply.github.com>
2022-11-04 14:33:01 -0500

    Update agg.sql

--- scripts/sql/agg.sql
+++ scripts/sql/agg.sql
@@ -16,5 +16,17 @@ FROM (SELECT *, row_number() over (partition BY pitcherid) AS seqnum,
 group by pitcherid, pitcher;
 
 #join to play by play to see result
+select *
+from dbo.pitches t1
+inner join dbo.pitch_tracking t2 
+    on t1.game_pk = t2.game_pk
+    and t1.pitcher_id = t2.pitcherid
+    and t1.batter_id = t2.batterid
+    and t1.pitch_number = t2.pitchno
+left join dbo.play_by_play t3 
+    on t1.game_pk = t2.game_pk
+    and t1.pitcher_id = t2.pitcherid
+    and t1.batter_id = t2.batterid
+    and t1.at_bat_index = t2.at_bat_index
 
 #join to pitches to see v/h_score
