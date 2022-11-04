6d045b96c4ea4bad9359baa5c70d3fe81d15b52f
cmill25 <44711910+cmill25@users.noreply.github.com>
2022-11-04 16:16:59 -0500

    Update null_tracking_data.sql

--- scripts/sql/null_tracking_data.sql
+++ scripts/sql/null_tracking_data.sql
@@ -1,6 +1,13 @@
-SELECT * FROM table1 
-WHERE NOT (Column1 IS NULL OR 
-Column2 IS NULL OR
- Column3 IS NULL OR
- Column4 IS NULL
+-- Returns rows which have null quantitative data for pitch tracking.
+
+SELECT * FROM dbo.tracking
+WHERE NOT ( 
+	relspeed IS NULL OR 
+	spinrate IS NULL OR
+	relheight IS NULL OR
+	relside IS NULL OR
+	extension IS NULL OR
+	vertbreak IS NULL OR
+	inducedvertbreak IS NULL OR
+	horzbreak IS NULL
     IS NOT NULL)
