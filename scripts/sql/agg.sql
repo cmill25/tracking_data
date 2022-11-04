30b5cfedc23f2421064feeb877dba0b381bbaaeb
cmill25 <44711910+cmill25@users.noreply.github.com>
2022-11-04 10:34:47 -0500

    Update agg.sql

--- scripts/sql/agg.sql
+++ scripts/sql/agg.sql
@@ -1,6 +1,20 @@
-select pitcherid, pitcher, avg(spinrate) as AverageSpinRate
-from (select *, row_number() over (partition by pitcherid) as seqnum,
-             count(*) over (partition by pitcherid) as cnt
-      from test.tracking
+#agg avarage pitching stuff
+SELECT pitcherid,
+  pitcher,
+  avg(relspeed) as AverageReleaseSpeed,
+  avg(spinrate) as AverageSpinRate,
+  avg(relheight) as AverageReleHeight,
+  avg(relside) as AverageRelSide,
+  avg(extension) as AverageExtension,
+  avg(vertbreak) as AverageVertBreak,
+  avg(inducedvertbreak) as AverageInducedVartBreak,
+  avg(horzbreak) as AverageHorzBreak
+FROM (SELECT *, row_number() over (partition BY pitcherid) AS seqnum,
+             COUNT(*) over (partition BY pitcherid) AS cnt
+      FROM test.tracking
      ) s
 group by pitcherid, pitcher;
+
+#join to play by play to see result
+
+#join to pitches to see v/h_score
